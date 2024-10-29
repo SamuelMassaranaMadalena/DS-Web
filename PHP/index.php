0d00c42d332bc804a1a051141fc15aadbf949499
SamuelMassaranaMadalena <samuelmassmada@gmail.com>
2024-10-29 16:18:09 -0300

    grande adição agora com php e microcurso

---
+++ PHP/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+</head>
+<body>
+    <h1 align="center">Sistema Login</h1>
+    <form action="login.php" method="post">
+        <label for="us">Usuário:</label>
+        <input type="text" name="user" id="us">
+        <label for="pass">Senha</label>
+        <input type="password" name="pswrd" id="pass">
+        <input type="submit" name="enviar" id="send">
+    </form>
+</body>
+</html>
